0cfbd178bed36557db87806d48bd0db1bdb8a014
Vidhya Parmeshwar Kadam <131935689+vidhyakadam@users.noreply.github.com>
2023-09-21 13:07:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
 //your code here
+// Store references to the divs and their initial backgrounds
+const divs = document.querySelectorAll('.image');
+let initialBackgrounds = [];
+
+// Initialize the initial backgrounds array
+divs.forEach((div) => {
+  initialBackgrounds.push(getComputedStyle(div).backgroundImage);
+});
+
+// Function to handle drag start
+function handleDragStart(event) {
+  event.dataTransfer.setData('text/plain', event.target.id);
+}
+
+// Function to handle drag over
+function handleDragOver(event) {
+  event.preventDefault();
+}
+
+// Function to handle drop
+function handleDrop(event) {
+  event.preventDefault();
+  const sourceId = event.dataTransfer.getData('text/plain');
+  const targetId = event.target.id;
+
+  // Swap backgrounds of the source and target divs
+  const sourceIndex = sourceId.slice(-1) - 1;
+  const targetIndex = targetId.slice(-1) - 1;
+  divs[sourceIndex].style.backgroundImage = getComputedStyle(divs[targetIndex]).backgroundImage;
+  divs[targetIndex].style.backgroundImage = initialBackgrounds[sourceIndex];
+}
+
+// Add event listeners for drag-and-drop functionality
+divs.forEach((div) => {
+  div.addEventListener('dragstart', handleDragStart);
+  div.addEventListener('dragover', handleDragOver);
+  div.addEventListener('drop', handleDrop);
+});
+
